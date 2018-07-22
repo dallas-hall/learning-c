0c84c8fb52b02451d0c03bc5ed8bb279dd46cbb3
Dallas Hall <mr.dallas.hall@gmail.com>
2018-07-22 18:09:45 +1000

    Rewrote using for loop.

---
+++ The_ANSI_C_Programming_Language/Chapter01/celcius_fahrenheit_table_float3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+// import new int types
+#include <inttypes.h>
+
+// this method returns an int (return code) and takes no arguments
+int main(void)
+{
+	float c;
+
+	printf("C\tF\n");
+	for(c = 0; c <= 300; c = c + 20) {
+		printf("%1.1f\t%3.1f\n", c, (c * 9.0 / 5.0) + 32); // right justified
+	}
+
+	return 0;
+}
+
