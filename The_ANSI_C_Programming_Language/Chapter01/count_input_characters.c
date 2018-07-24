73305d051386619f921634356859488025a9a2d2
Dallas Hall <dallas.hall@example.com>
2018-07-24 20:23:30 +1000

    Moar garbage code.

---
+++ The_ANSI_C_Programming_Language/Chapter01/count_input_characters.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+int main(void)
+{
+	int32_t charCount;
+	charCount = 0;
+
+	printf("Enter some text. The characeters will be counted and displayed at the end.\n");
+	while(EOF != getchar()) {
+		++charCount;
+	}
+	printf("You typed %1d characters including the newline(s).\n", charCount);
+	return 0;
+}
