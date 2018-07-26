2a992e2ec31c5039737021ae0ca5fd7dc725410c
Dallas Hall <dallas.hall@example.com>
2018-07-26 21:11:10 +1000

    Program to count spaces, tabs, and newlines inside of input.

---
+++ The_ANSI_C_Programming_Language/Chapter01/count_input_spaces_tabs_lines.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+int main(void)
+{
+	int32_t currentChar, spaceCount, tabCount, lineCount;
+	spaceCount = 0;
+	tabCount = 0;
+	lineCount = 0;
+
+	printf("This program will count spaces, tabs, and lines.\n");
+	printf("Type some lines and press control+d when finished.\n");
+
+	while(EOF != (currentChar = getchar())) {
+		if (' ' == currentChar) {
+			spaceCount++;
+		}
+		else if ('\t' == currentChar) {
+			tabCount++;
+		}
+		if ('\n' == currentChar) {
+			lineCount++;
+		}
+	}
+	printf("The number of spaces types was %d\n", spaceCount);
+	printf("The number of tabs types was %d\n", tabCount);
+	printf("The number of lines types was %d\n", lineCount);
+	return 0;
+}
