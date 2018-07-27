afa0cf5c27634d1aded234ebe4e31c728009750c
Dallas Hall <dallas.hall@example.com>
2018-07-27 20:37:31 +1000

    Primitive wc program. Counts lines, words, and characters.

---
+++ The_ANSI_C_Programming_Language/Chapter01/count_input_lines_words_characters.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+// controls wordState, which is used to keep track of the start and end of words.
+static const int8_t START = 1;
+static const int8_t END = 0;
+
+int main(void)
+{
+	int32_t currentChar, numberLines, numberWords, numberChars, wordState;
+	wordState = END;
+	numberLines = 0;
+	numberWords = 0;
+	numberChars = 0;
+
+	printf("This program will count the number of lines, words, and characters of the text you enter.\n");
+	printf("Enter some text and press control+d to exit\n");
+	while(EOF != (currentChar = getchar())) {
+		numberChars++;
+		if ('\n' ==  currentChar) {
+			numberLines++;
+		}
+		if (' ' == currentChar || '\t' == currentChar || '\n' == currentChar) {
+			wordState = END;
+		}
+		else if (END == wordState) {
+			wordState = START;
+			numberWords++;
+		}
+	}
+
+	printf("Lines: %d Words: %d Characters: %d\n", numberLines, numberWords, numberChars);
+	return 0;
+}
