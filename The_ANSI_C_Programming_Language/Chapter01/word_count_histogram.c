154117b0e0995ba549625e14e05c4648a878d698
Dallas Hall <dallas.hall@example.com>
2018-07-28 22:34:25 +1000

    Vertical histogram done.

--- The_ANSI_C_Programming_Language/Chapter01/word_count_histogram.c
+++ The_ANSI_C_Programming_Language/Chapter01/word_count_histogram.c
@@ -17,7 +17,8 @@ int main(void)
 	printf("This program will count the length of words and create a horizontal histogram.\nType control+d to exit.\n");
 
 	while (EOF != (currentChar = getchar())) {
-		if (' ' == currentChar || '\n' == currentChar || '\t' == currentChar) {
+		if (' ' == currentChar || '\n' == currentChar || '\t' == currentChar ||
+			',' == currentChar || '.' == currentChar) {
 			wordLengths[currentWordLength - 1]++;
 			currentWordLength = 0;
 		}
@@ -31,7 +32,7 @@ int main(void)
 		printf("Word length %d occured %d times.\n", i + 1, wordLengths[i]); 
 	}
 
-	printf("[INFO] Sideways histogram.\n");
+	printf("[INFO] Horizontal histtogram.\n");
 	for (int i = 0; i < maximumWordLength; i++) {
 		printf("%d: ", i + 1); 
 		for (int j = 0; j < wordLengths[i]; j++) {
@@ -40,5 +41,27 @@ int main(void)
 		printf("\n");
 	}
 
+	printf("[INFO] Vertical histogram.\n");
+	int8_t length = maximumWordLength - 1;
+	// = is needed since the number is 15.
+	for (int i = length; i >= 0; i--) {
+		for (int j = 0; j <= length; j++) {
+			if (0 == wordLengths[j] || i >= wordLengths[j]) {
+				printf("   ");
+			}
+			else {
+				printf(" * ");
+			}
+		}
+		printf("\n");
+		// if i = 0 print the row
+		if (0 == i) {
+			for (int k = 0; k < maximumWordLength; k++) {
+				printf("%2d ", k + 1);
+			}
+		}
+	}
+
+	printf("\n");
 	return 0;
 }
