4a32284f5e1b32d75145db2f86338ab6614abf5e
Dallas Hall <dallas.hall@example.com>
2018-08-11 23:54:47 +1000

    C99 signed and unsigned, C11 signed and unsigned for normal, fast, and least number ranges printed.

--- The_ANSI_C_Programming_Language/Chapter02/print_number_ranges.c
+++ The_ANSI_C_Programming_Language/Chapter02/print_number_ranges.c
@@ -37,19 +37,27 @@ int main(void)
 
 	printf("The C99 SIGNED number ranges are:\n");
 	// %d prints out a signed integer
-	printf("char min:\t%d\t\nchar max:\t%d\n", char_min, char_max);
-	printf("short min:\t%d\t\nshort max:\t%d\n", short_min, short_max);
-	printf("int min:\t%d\t\nint max:\t%d\n", int_min, int_max);
+	printf("char min:\t%d\n", char_min);
+	printf("char max:\t%d\n", char_max);
+	printf("short min:\t%d\n", short_min);
+	printf("short max:\t%d\n", short_max);
+	printf("int min:\t%d\n", int_min);
+	printf("int max:\t%d\n", int_max);
 	// %ld prints out a signed long
-	printf("long min:\t%ld\t\nlong max:\t%ld\n\n", long_min, long_max);
+	printf("long min:\t%ld\n", long_min);
+	printf("long max:\t%ld\n", long_max);
 
-	printf("The C99 UNSIGNED number ranges are:\n");
+	printf("\nThe C99 UNSIGNED number ranges are:\n");
 	// %u prints out an unsigned integer
-	printf("char min:\t%u\t\nchar max:\t%u\n", uchar_min, uchar_max);
-	printf("short min:\t%u\t\nshort max:\t%u\n", ushort_min, ushort_max);
-	printf("int min:\t%u\t\nint max:\t%u\n", uint_min, uint_max);
+	printf("uchar min:\t%u\n", uchar_min);
+	printf("uchar max:\t%u\n", uchar_max);
+	printf("ushort min:\t%u\n", ushort_min);
+	printf("ushort max:\t%u\n", ushort_max);
+	printf("uint min:\t%u\n", uint_min);
+	printf("uint max:\t%u\n", uint_max);
 	// %ld prints out an unsigned long
-	printf("long min:\t%lu\t\nlong max:\t%lu\n\n", ulong_min, ulong_max);
+	printf("ulong min:\t%lu\n", ulong_min);
+	printf("ulong max:\t%lu\n", ulong_max);
 
 	// @@@ C11 Standard @@@
 	// https://en.cppreference.com/w/c/types/integer
@@ -120,29 +128,93 @@ int main(void)
 	uint_least8_t uint_least8_t_min, uint_least8_t_max;
 	uint_least16_t uint_least16_t_min, uint_least16_t_max;
 	uint_least32_t uint_least32_t_min, uint_least32_t_max;
-	uint_least64_t uint_least64_t_min, uint_least64_t_max; 
+	uint_least64_t uint_least64_t_min, uint_least64_t_max;
 
-	printf("The C11 SIGNED number ranges are:\n");
+	int_least8_t_min = INT_LEAST8_MIN;
+	int_least8_t_max = INT_LEAST8_MAX;
+	int_least16_t_min = INT_LEAST16_MIN;
+	int_least16_t_max = INT_LEAST16_MAX;
+	int_least32_t_min = INT_LEAST32_MIN;
+	int_least32_t_max = INT_LEAST32_MAX;
+	int_least64_t_min = INT_LEAST64_MIN;
+	int_least64_t_max = INT_LEAST64_MAX;
+
+	uint_least8_t_min = 0;
+	uint_least8_t_max = UINT_LEAST8_MAX;
+	uint_least16_t_min = 0;
+	uint_least16_t_max = UINT_LEAST16_MAX;
+	uint_least32_t_min = 0;
+	uint_least32_t_max = UINT_LEAST32_MAX;
+	uint_least64_t_min = 0;
+	uint_least64_t_max = UINT_LEAST64_MAX;
+	
+
+	printf("\nThe C11 SIGNED number ranges are:\n");
 	// PRId prints out a signed integer with the corresponding bit amount.  
-	printf("int8_t min:\t%" PRId8 "\t\nint8_t max:\t%" PRId8 "\n", int8_t_min, int8_t_max);
-	printf("int16_t min:\t%" PRId16 "\t\nint16_t max:\t%" PRId16 "\n", int16_t_min, int16_t_max);
-	printf("int32_t min:\t%" PRId32 "\t\nint32_t max:\t%" PRId32 "\n", int32_t_min, int32_t_max);
-	printf("int64_t min:\t%" PRId64 "\t\nint64_t max:\t%" PRId64 "\n\n", int64_t_min, int64_t_max);
+	printf("int8_t min:\t%" PRId8 "\n", int8_t_min);
+	printf("int8_t max:\t%" PRId8 "\n", int8_t_max);
+	printf("int16_t min:\t%" PRId16 "\n", int16_t_min);
+	printf("int16_t max:\t%" PRId16 "\n", int16_t_max);
+	printf("int32_t min:\t%" PRId32 "\n", int32_t_min);
+	printf("int32_t max:\t%" PRId32 "\n", int32_t_max);
+	printf("int64_t min:\t%" PRId64 "\n", int64_t_min);
+	printf("int64_t max:\t%" PRId64 "\n", int64_t_max);
 
-	printf("The C11 UNSIGNED number ranges are:\n");
+	printf("\nThe C11 UNSIGNED number ranges are:\n");
 	// PRIu prints out an unsigned integer with the corresponding bit amount.  
-	printf("uint8_t min:\t%" PRIu8 "\t\nuint8_t max:\t%" PRIu8 "\n", uint8_t_min, uint8_t_max);
-	printf("uint16_t min:\t%" PRIu16 "\t\nuint16_t max:\t%" PRIu16 "\n", uint16_t_min, uint16_t_max);
-	printf("uint32_t min:\t%" PRIu32 "\t\nuint32_t max:\t%" PRIu32 "\n", uint32_t_min, uint32_t_max);
-	printf("uint64_t min:\t%" PRIu64 "\t\nuint64_t max:\t%" PRIu64 "\n\n", uint64_t_min, uint64_t_max);
+	printf("uint8_t min:\t%" PRIu8 "\n", uint8_t_min);
+	printf("uint8_t max:\t%" PRIu8 "\n", uint8_t_max);
+	printf("uint16_t min:\t%" PRIu16 "\n", uint16_t_min);
+	printf("uint16_t max:\t%" PRIu16 "\n", uint16_t_max);
+	printf("uint32_t min:\t%" PRIu32 "\n", uint32_t_min);
+	printf("uint32_t max:\t%" PRIu32 "\n", uint32_t_max);
+	printf("uint64_t min:\t%" PRIu64 "\n", uint64_t_min);
+	printf("uint64_t max:\t%" PRIu64 "\n", uint64_t_max);
 
-	printf("The C11 SIGNED FAST number ranges are:\n");
+
+	printf("\nThe C11 SIGNED FAST number ranges are:\n");
+	// PRIu prints out a signed integer with the corresponding bit amount.  
+	printf("int_fast8_t min:\t%" PRIdFAST8 "\n", int_fast8_t_min);
+	printf("int_fast8_t maxn:\t%" PRIdFAST8 "\n", int_fast8_t_max);
+	printf("int_fast16_t min:\t%" PRIdFAST16 "\n", int_fast16_t_min);
+	printf("int_fast16_t maxn:\t%" PRIdFAST16 "\n", int_fast16_t_max);
+	printf("int_fast32_t min:\t%" PRIdFAST32 "\n", int_fast32_t_min);
+	printf("int_fast32_t maxn:\t%" PRIdFAST32 "\n", int_fast32_t_max);
+	printf("int_fast64_t min:\t%" PRIdFAST64 "\n", int_fast64_t_min);
+	printf("int_fast64_t maxn:\t%" PRIdFAST64 "\n", int_fast64_t_max);
+
+	printf("\nThe C11 UNSIGNED FAST number ranges are:\n");
 	// PRIu prints out an unsigned integer with the corresponding bit amount.  
-	printf("uint8_t min:\t%" PRIu8 "\t\nuint8_t max:\t%" PRIu8 "\n", uint8_t_min, uint8_t_max);
-	printf("uint16_t min:\t%" PRIu16 "\t\nuint16_t max:\t%" PRIu16 "\n", uint16_t_min, uint16_t_max);
-	printf("uint32_t min:\t%" PRIu32 "\t\nuint32_t max:\t%" PRIu32 "\n", uint32_t_min, uint32_t_max);
-	printf("uint64_t min:\t%" PRIu64 "\t\nuint64_t max:\t%" PRIu64 "\n\n", uint64_t_min, uint64_t_max);
+	printf("uint_fast8_t min:\t%" PRIuFAST8 "\n", uint_fast8_t_min);
+	printf("uint_fast8_t maxn:\t%" PRIuFAST8 "\n", uint_fast8_t_max);
+	printf("uint_fast16_t min:\t%" PRIuFAST16 "\n", uint_fast16_t_min);
+	printf("uint_fast16_t maxn:\t%" PRIuFAST16 "\n", uint_fast16_t_max);
+	printf("uint_fast32_t min:\t%" PRIuFAST32 "\n", uint_fast32_t_min);
+	printf("uint_fast32_t maxn:\t%" PRIuFAST32 "\n", uint_fast32_t_max);
+	printf("uint_fast64_t min:\t%" PRIuFAST64 "\n", uint_fast64_t_min);
+	printf("uint_fast64_t maxn:\t%" PRIuFAST64 "\n", uint_fast64_t_max);
 
+	printf("\nThe C11 SIGNED LEAST number ranges are:\n");
+	// PRIu prints out a signed integer with the corresponding bit amount.  
+	printf("int_least8_t min:\t%" PRIdLEAST8 "\n", int_least8_t_min);
+	printf("int_least8_t maxn:\t%" PRIdLEAST8 "\n", int_least8_t_max);
+	printf("int_least16_t min:\t%" PRIdLEAST16 "\n", int_least16_t_min);
+	printf("int_least16_t maxn:\t%" PRIdLEAST16 "\n", int_least16_t_max);
+	printf("int_least32_t min:\t%" PRIdLEAST32 "\n", int_least32_t_min);
+	printf("int_least32_t maxn:\t%" PRIdLEAST32 "\n", int_least32_t_max);
+	printf("int_least64_t min:\t%" PRIdLEAST64 "\n", int_least64_t_min);
+	printf("int_least64_t maxn:\t%" PRIdLEAST64 "\n", int_least64_t_max);
+
+	printf("\nThe C11 UNSIGNED LEAST number ranges are:\n");
+	// PRIu prints out an unsigned integer with the corresponding bit amount.  
+	printf("uint_least8_t min:\t%" PRIuLEAST8 "\n", uint_least8_t_min);
+	printf("uint_least8_t maxn:\t%" PRIuLEAST8 "\n", uint_least8_t_max);
+	printf("uint_least16_t min:\t%" PRIuLEAST16 "\n", uint_least16_t_min);
+	printf("uint_least16_t maxn:\t%" PRIuLEAST16 "\n", uint_least16_t_max);
+	printf("uint_least32_t min:\t%" PRIuLEAST32 "\n", uint_least32_t_min);
+	printf("uint_least32_t maxn:\t%" PRIuLEAST32 "\n", uint_least32_t_max);
+	printf("uint_least64_t min:\t%" PRIuLEAST64 "\n", uint_least64_t_min);
+	printf("uint_least64_t maxn:\t%" PRIuLEAST64 "\n", uint_least64_t_max);
 
 	return 0;
 }
