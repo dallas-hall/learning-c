10e14a267a83872320364a3e3e03634056a53580
Dallas Hall <mr.dallas.hall@gmail.com>
2019-03-29 08:31:08 +1100

    Added the explanation for ^D and removed new int types so the example matches the book.

--- The-ANSI-C-Programming-Language/Chapter01/copy-input-to-output4.c
+++ The-ANSI-C-Programming-Language/Chapter01/copy-input-to-output4.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <inttypes.h>
 
 int main(void)
 {
 	printf("Enter some text via stdin which is read in and printed to stdout.\n");
 	printf("1 will be printed for each non-EOF char.\n");
-	printf("Press ctrl+D to send an EOF character.\n");	
-	int16_t c;
+	printf("Press ^D (ctrl + D) to send an EOF character.\n");	
+	int c;
 
 	/*
 	Put the getchar() call inside the while loop. This needs to be inside parentheses.
@@ -19,7 +18,7 @@ int main(void)
 		// Prints out ASCII code point for non-EOF chars
 		printf("%d%s", c, "\n");
 	}
-	// -1 for EOF chars.	
+	// -1 for EOF chars.
 	printf("%d%s", c, "\n");
 	return 0;
 }
