612c256f808bb15859577a1a0daf8a68190cbde7
Dallas Hall <mr.dallas.hall@gmail.com>
2019-03-28 19:10:19 +1100

    Added in #define which is the old way to define constants.

--- UNSW-Richard-Buckland-C-course/Week02.c
+++ UNSW-Richard-Buckland-C-course/Week02.c
@@ -8,6 +8,9 @@
 
 // Tell the compiler to include this file at this point in the source code. This is a way to import libraries and their code.
 #include <stdio.h>
+// This means the copmiler rewrites the constant with the value
+#define LENGTH 2
+#define WIDTH 4
 
 //The main function is called by the operating system. It should return at int to the O/S to tell it if the program was successful or not. 0 = successful.
 int main(int argc, char *argv[])
@@ -22,12 +25,15 @@ int main(int argc, char *argv[])
 	// Read in the user input
 	scanf("%d", &input);
 	
-	dollars = input / 2;
+	dollars = (input) / 2;
 	// When integer division is performed on an odd number, the decimal is component is thrown away. This is essentially division with remainders and ignoring the remainder.
 	integerDivision = input % 2;
 
 	printf("Integer division: %d / 2 is $%d.\n", input, dollars);
 	// Notice the double % to type a literal %
 	printf("Integer modulo: %d %% 2 is $%d.\n", input, integerDivision);
+	printf("The constant LENGTH is %d\nThe constant WIDTH is %d\n", LENGTH, WIDTH);
+	int area = LENGTH * WIDTH;
+	printf("The area of %d * %d is %d\n", LENGTH, WIDTH, area);
 	return 0;
 }
