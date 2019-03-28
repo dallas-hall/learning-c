b120b021f41c077daad8a6deb8ccd023b40431d1
Dallas Hall <mr.dallas.hall@gmail.com>
2019-03-29 08:13:15 +1100

    Added the explanation for ^D and removed new int types so the example matches the book.

--- The-ANSI-C-Programming-Language/Chapter01/copy-input-to-output2.c
+++ The-ANSI-C-Programming-Language/Chapter01/copy-input-to-output2.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#include <inttypes.h>
 
 int main(void)
 {
 	printf("Enter some text via stdin which is read in and printed to stdout.\n");
-	printf("Press ctrl+D to send an EOF character.\n");	
-	int16_t c;
+	printf("Press ^D (ctrl + D) to send an EOF character.\n");	
+	// int is used so we can hold the character value and EOF.
+	int c;
 
 	/*
-	Pur the getchar() call inside the while loop. This needs to be inside parentheses.
+	Put the getchar() call inside the while loop. This needs to be inside parentheses.
 	!= has a higher precedence than =, so without the parentheses the relational test
 	is done before the assignment statement. This would set the variable to being
 	either 0 or 1, depending on if EOF was returned.
