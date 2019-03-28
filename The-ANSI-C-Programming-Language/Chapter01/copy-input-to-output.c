7439e4a0d7aae5f02e7b45a9e973f7acb9f0d733
Dallas Hall <dallas.hall@example.com>
2019-03-29 08:03:49 +1100

    Added the explanation for ^D and removed new int types so the example matches the book.

--- The-ANSI-C-Programming-Language/Chapter01/copy-input-to-output.c
+++ The-ANSI-C-Programming-Language/Chapter01/copy-input-to-output.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <inttypes.h>
 
 int main(void)
 {
 	printf("Enter some text via stdin which is read in and printed to stdout.\n");
-	printf("Press ctrl+D to send an EOF character.\n");	
-	int16_t c;
+	printf("Press ^D (ctrl + D) to send an EOF character.\n");	
+	int c;
 	c = getchar();
 
 	while(EOF != c) {
