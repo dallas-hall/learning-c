47b54e43d03c8af364b33bec14c78d9fab6d91aa
Dallas Hall <dallas.hall@example.com>
2019-03-29 13:39:04 +1100

    Updated for ^D.

--- The-ANSI-C-Programming-Language/Chapter01/copy-input-to-output5.c
+++ The-ANSI-C-Programming-Language/Chapter01/copy-input-to-output5.c
@@ -4,7 +4,7 @@
 int main(void)
 {
 	printf("Enter some text via stdin which is read in and printed to stdout.\n");
-	printf("Press ctrl+D to send an EOF character.\n");	
+	printf("Press ^D (ctrl + D) to send an EOF character.\n");	
 	
 	int32_t c;
 	char line[256];
@@ -15,12 +15,12 @@ int main(void)
 	}
 
 	/*
-	Pur the getchar() call inside the while loop. This needs to be inside parentheses.
+	Put the getchar() call inside the while loop. This needs to be inside parentheses.
 	!= has a higher precedence than =, so without the parentheses the relational test
 	is done before the assignment statement. This would set the variable to being
 	either 0 or 1, depending on if EOF was returned.
 	*/
-	int	i = 0;
+	int i = 0;
 	while(EOF != (c = getchar())) {
 		line[i] = c;
 		i++;
