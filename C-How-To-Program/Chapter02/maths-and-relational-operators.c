c4e3d89db90ae5a72256961564c1544da58cabfc
Dallas Hall <dallas.hall@example.com>
2019-05-27 19:42:42 +1000

    Displaying the maths and relational operators.

---
+++ C-How-To-Program/Chapter02/maths-and-relational-operators.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/*
+	 * Note the double percent for a literal percent sign.
+	 * Note the double backslash for a literal backslash.
+	 * There is no exponenation syntax in C, need to use the Standard Library pow function.
+	 *
+	 */
+	printf("# Arithmetic Operators\nModulo is %%\nMultiplication is *\nDivision is \\\nAddition is +\nSubtraction is -");
+	printf("\n\n# Relational Operators\nGreater than or equal to is >=\nGreater than is >\nLess than or equal to is <=\nLess than is <\nEquals is ==\nNot equals is !=\n");
+	return 0;
+}
