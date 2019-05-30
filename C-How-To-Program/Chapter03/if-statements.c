6a1e83cf52942f564b390914d8ed85733064fb5c
Dallas Hall <mr.dallas.hall@gmail.com>
2019-05-30 19:22:20 +1000

    If statements yo.

--- C-How-To-Program/Chapter03/if-statements.c
+++ C-How-To-Program/Chapter03/if-statements.c
@@ -13,7 +13,7 @@ int main(void)
 		printf("Score is: %d\n", result);
 	}
 
-	printf("\n# Selection Statements\n## if Statement\n");
+	printf("\n# If Selection Statements\n## if Statement\n");
 	if (result >= 50) {
 		printf("You passed.\n");
 	}
@@ -52,32 +52,6 @@ int main(void)
 
 	printf("\n## Tenary Conditional Operator Statement\n");
     printf("%s\n", result >= 50 ? "You passed.\n" : "You failed.\n");
-
-	printf("## Switch Statement\n");
-	switch(code) {
-		case 4:
-		printf("You passed and got a high distinction.\n");
-		break;
-
-		case 3:
-		printf("You passed and got a distinction.\n");
-		break;
-	
-		case 2:
-		printf("You passed and got a credit.\n");
-		break;
-	
-		case 1:
-		printf("You passed.\n");
-		break;
-	
-		case 0:
-		printf("You failed.\n");
-		break;
-	
-		default:
-		printf("Invalid entry.\n");
-	}
 
 	return 0;
 }	
