3c958990746381aa58afc9d8b8dc1e7a150577b8
Dallas Hall <dallas.hall@example.com>
2019-06-05 18:51:28 +1000

    Added some more simple integer comparisons.

--- C-How-To-Program/Chapter02/integer-comparison.c
+++ C-How-To-Program/Chapter02/integer-comparison.c
@@ -3,7 +3,9 @@
 
 int main(void)
 {
-	int a, b;
+	int a, b, c, sum, product, smallest, largest;
+	float average;
+
 	printf("Enter 2 integers separated by space: ");
 	scanf("%d %d", &a, &b);
 	if (a > b) {
@@ -15,6 +17,85 @@ int main(void)
 	else {
 		printf("%d < %d.\n", a, b);
 	}
+
+	printf("Enter 1 more integer: ");
+	scanf("%d", &c);
+	sum = a + b + c;
+	product = a * b * c;
+	
+	if (a > b && a > c) {
+		largest = a;
+	}
+	else if (b > a && b > c) {
+		largest = b;
+	}
+	else if (c > a && c > b) {
+		largest = c;
+	}
+	/* Take the first as the "largest" */
+	else
+	{
+		if (a == b && a > c) {
+			largest = a;
+		}
+		else if (a == c && a > b) {
+			largest = a;
+		}
+		/*else if (b == a && b > c) {
+			largest = b;
+		}*/
+		else if (b == c && b > a) {
+			largest = b;
+		}
+		/*else if (c == a && c > b) {
+			largest = c;
+		}
+		else if (c == b && c > a) {
+			largest = c;
+		}*/
+	}
+
+	if (a < b && a < c) {
+		smallest = a;
+	}
+	else if (b < a && b < c) {
+		smallest = b;
+	}
+	else if (c < a && c < b) {
+		smallest = c;
+	}
+	/* Take the first as the "smallest" */
+	else {
+		if (a == b && a < c) {
+			smallest = a;
+		}
+		else if (a == c && a < b) {
+			smallest = a;
+		}
+		/*else if (b == a && b < c) {
+			smallest = b;
+		}*/
+		else if (b == c && b < a) {
+			smallest = b;
+		}
+		/*else if (c == a && c < b) {
+			smallest = c;
+		}
+		else if (c == b && c < a) {
+			smallest = c;
+		}*/
+	
+	}
+
+	sum = a + b + c;
+	product = a * b * c;
+	average = (float) sum / 3;
+
+	printf("The largest of %d, %d, and %d is %d.\n", a, b, c, largest);
+	printf("The smallest of %d, %d, and %d is %d.\n", a, b, c, smallest);
+	printf("The sum of %d + %d + %d is %d.\n", a, b, c, sum);
+	printf("The product of %d * %d * %d is %d.\n", a, b, c, product);
+	printf("The average of (%d + %d + %d) / 3 is %.2f.\n", a, b, c, average);
 
 	return EXIT_SUCCESS;
 }
