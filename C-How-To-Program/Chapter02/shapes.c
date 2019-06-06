99be601719774ed31f36fcfe75417930195e8f83
Dallas Hall <mr.dallas.hall@gmail.com>
2019-06-06 12:20:04 +1000

    Simplified the for loop inside of the circle and added a fixed sized arrow.

--- C-How-To-Program/Chapter02/shapes.c
+++ C-How-To-Program/Chapter02/shapes.c
@@ -38,48 +38,59 @@ int main(void)
 			// These magic numbers came from tweaking after viewing output. Works reasonably well.
 			outerSpaces = (int) (width * .25);
 			innerSpaces = 0;
-			stars = (int) (width * .66);
-
-			for(int j = 0; j < outerSpaces; j++) {
-				printf(" ");
-			}
-			for(int k = 0; k < stars; k++) {
-				printf("*");
-			}
+			stars = (int) (width * .66) - 1;
+			
 		}
 		else if(i == 1 || i == height - 2) {
 			outerSpaces = 1;
 			innerSpaces = width - 4; // To remove 2 stars and 2 outter spaces
 			stars = 1;
-			for(int j = 0; j < outerSpaces; j++) {
-				printf(" ");
-			}
-			for(int k = 0; k < stars; k++) {
-				printf("*");
-			}
-			for(int l = 0; l < innerSpaces; l++) {
-				printf(" ");
-			}
-			printf("*");
 		}
 		else {
 			outerSpaces = 0;
 			innerSpaces = width - 2;
 			stars = 1;
-			for(int j = 0; j < stars; j++) {
-				printf("*");
-			}
-			for(int k = 0; k < innerSpaces; k++) {
-				printf(" ");
-			}
+		}
+		for(int j = 0; j < outerSpaces; j++) {
+			printf(" ");
+		}
+		for(int k = 0; k < stars; k++) {
 			printf("*");
 		}
-
-		printf("\n");
+		for(int l = 0; l < innerSpaces; l++) {
+			printf(" ");
+		}
+	
+		printf("*\n");
 	}
 
 	printf("# Printing shape 3, an arrow.\n");
-	
+	for(int i = 0; i < height; i++) {
+		if(i == 0) {
+			outerSpaces = width / 2;
+			innerSpaces = 0;
+			stars = 1;
+		}
+		else if(i == 1) {
+			outerSpaces = (width / 2) - 1;
+			stars = 3;
+		}
+		else if(i == 2) {
+			outerSpaces = (width / 2) - 2;
+			stars = 5;
+		}
+		else {
+			outerSpaces = (width / 2);
+			stars = 1;
+		}
+		for(int j = 0; j < outerSpaces; j++) {
+			printf(" ");
+		}
+		for(int k = 0; k < stars; k++) {
+			printf("*");
+		}
+		printf("\n");
+	}
 
 	return EXIT_SUCCESS;
 }
