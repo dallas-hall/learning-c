4b3a0692799623f5cd74ed8092bebbd90b08dc98
Dallas Hall <mr.dallas.hall@gmail.com>
2019-06-14 08:20:34 +1000

    Calculating factorial numbers using recursion. Updated logic to properly handle 0.

--- C-How-To-Program/Chapter05/factorial-recursion.c
+++ C-How-To-Program/Chapter05/factorial-recursion.c
@@ -10,11 +10,10 @@ int main(void)
 	printf("# Calculating Factorial - Recursion Example\nEnter a positive integer: ");
 	scanf("%d", &input);
 
-	if(input <= 0) {
+	if(input < 0) {
 		printf("Can only calculate factorials for positve integers.\n");
 	}
 	else {
-
 		printf("The factorial of %d (%d!) is %d\n", input, input, factorial(input));
 	}
 	return EXIT_SUCCESS;
@@ -25,8 +24,19 @@ int factorial(int n)
 	int recursionResult, finalResult;
 	finalResult = n;
 
-	/* Base case, n is 1. So only process when not 1 */
-	if (1 != n) {
+	/*
+	 * Base case, n is 1 or 0. So only process when not 1 or 0
+	 */
+	if (0 == n || 1 == n) {
+		finalResult = 1;
+	}
+	else {
+		/*
+		 * Make a recursive call with n - 1.
+		 * This means we will eventually get to n == 1, the base case.
+		 * Once the base case is reached, the stack is unwound and each stack frame returns its result.
+		 * The results are continually multiplied together to produce the final product, which is the last value returned.
+		 */ 
 		recursionResult = factorial(n - 1);
 		finalResult *= recursionResult;
 	}
