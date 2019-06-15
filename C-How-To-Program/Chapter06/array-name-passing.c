5caf1202ac491c7cf0701a66368ad9a6057cee74
Dallas Hall <mr.dallas.hall@gmail.com>
2019-06-15 13:17:39 +1000

    Showing the how the array's variable name, &name, and &name[0] are actually pointers to the same memory location.

---
+++ C-How-To-Program/Chapter06/array-name-passing.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+	/* String literal plus the '\0' is stored. */
+	char array[] = "Hello World!";
+
+	printf("# Array Name Is Memory Location Example - variable called array\n");
+	/*
+	 * Remeber to cast &array to (void *), otherwise compiler implicitly does this.
+	 */ 
+
+	printf("array\t\t = %p\n&array\t\t = %p\n&array[0]\t = %p\n", array, (void *) &array, &array[0]);
+
+	return EXIT_SUCCESS;
+}
