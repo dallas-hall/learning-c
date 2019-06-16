533707533735d53b5dbd65f0821467d521157a23
Dallas Hall <dallas.hall@example.com>
2019-06-16 18:43:17 +1000

    Simple example of reading stdin into a buffer with getchar and writing out to stdout with puts

---
+++ C-How-To-Program/Chapter08/read-with-getchar-write-with-puts.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER 80
+
+int main(void)
+{
+	char input[BUFFER];
+	char c;
+	int i = 0;
+
+	puts("# Reading From stdin & Writing To stdout");
+	puts("## Read stdin With getchar\nEnter your message:");
+	/*
+	 * Read characters from stdin until a) newline b) EOF or c) buffer exhausted
+	 * Store these into an array with the size of BUFFER.
+	 */ 
+	while (EOF != (c = getchar()) || '\0' != (c = getchar())) {
+		input[i] = c;
+		i++;
+		if(i == BUFFER - 2) {
+			input[BUFFER - 1] = '\0';
+			break;
+		}
+	}
+
+	puts("\n## Write To stdout With puts");
+	puts(input);
+	return EXIT_SUCCESS;
+}
