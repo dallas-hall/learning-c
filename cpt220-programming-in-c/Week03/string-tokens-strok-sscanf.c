fb661e6bc4032f1fc0b1ca4c2b96f50a54399446
Dallas Hall <mr.dallas.hall@gmail.com>
2019-06-19 12:43:52 +1000

    Completed both strtok and sscanf

--- cpt220-programming-in-c/Week03/string-tokens-strok-sscanf.c
+++ cpt220-programming-in-c/Week03/string-tokens-strok-sscanf.c
@@ -5,11 +5,15 @@
 #define MAX_INPUT 18
 #define EXTRA_CHARS 2
 
+int debugging = 0;
+
 long getIntFromString(char s[])
 {
 	long li;
 	char *strPtr;
-	puts("getIntFromString call");
+	if(debugging) {
+		puts("getIntFromString call");
+	}
 
 	/* Does detect errors */
 	li = strtol(s, &strPtr, 10);
@@ -24,20 +28,27 @@ int sumDelimitedInts(char s[], char delimiter[])
 	int count;
 	char *tokenPtr;
 
-	puts("sumDelimitedInts call");
 	tokenPtr = strtok(s, delimiter);
-	printf("first token %s\n", tokenPtr);
+	if (debugging) {
+		puts("sumDelimitedInts call");
+		printf("first token %s\n", tokenPtr);
+	}
 	count = getIntFromString(tokenPtr);
 
 	while(tokenPtr != NULL) {
-		puts("another token");
+		if(debugging) {
+			puts("another token");
+		}
 		/*
 		 * The NULL argument tell strtok to keep going.
 		 * NULL is returned when there are no more tokens.
 		 */
 
 		tokenPtr = strtok(NULL, delimiter);
-		printf("next token %s\n", tokenPtr);
+		if(debugging) {
+			printf("next token %s\n", tokenPtr);
+		}
+
 		if(tokenPtr == NULL) {
 			break;
 		}
@@ -52,22 +63,30 @@ int sumDelimitedInts2(char s[], char delimiter)
 	int count, i, j;
 	char currentInt[MAX_INPUT] = {0};
 
-	puts("sumDelimitedInts2 call");
-	printf("del is %c\n", delimiter);
-	printf("s is %s\n", s);
+	if(debugging) {
+		puts("sumDelimitedInts2 call");
+		printf("del is %c\n", delimiter);
+		printf("s is %s\n", s);
+	}
 
 	count = 0;
 	j = 0;
 	for(i = 0; i < strlen(s); i++) {
-		printf("s[%d] is %c\n", i, s[i]);
+		if (debugging) {
+			printf("s[%d] is %c\n", i, s[i]);
+		}
 		if(s[i] == delimiter || s[i] == '\n') {
-			printf("all currentInt is %s\n", currentInt);
+			if(debugging) {
+				printf("all currentInt is %s\n", currentInt);
+			}
 			count += getIntFromString(currentInt);
 			j = 0;
 		}
 		else {
 			currentInt[j] = s[i];
-			printf("currentInt[%d] = %c\n", j, s[i]);
+			if(debugging) {
+				printf("currentInt[%d] = %c\n", j, s[i]);
+			}
 			j++;
 		}
 	}
@@ -109,10 +128,10 @@ int main(void)
 	puts("");
 
 	total = sumDelimitedInts(ints, delimiter);
-	printf("The total was %d\n", total);
+	printf("With strtok, the total was %d\n", total);
 
 	total = sumDelimitedInts2(ints2, delimiter[0]);
-	printf("The total was %d\n", total);
+	printf("With sscanf, the total was %d\n", total);
 
 	return EXIT_SUCCESS;
 
