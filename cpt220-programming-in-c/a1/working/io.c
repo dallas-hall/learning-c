c1b5866308759c8471c8082db68bbf10204c7a4b
Dallas Hall <mr.dallas.hall@gmail.com>
2019-06-21 14:10:55 +1000

    Fixed the pointer issue, players intialise with names properly now.

--- cpt220-programming-in-c/a1/working/io.c
+++ cpt220-programming-in-c/a1/working/io.c
@@ -7,21 +7,21 @@
  * study period 2, 2019.
  *****************************************************************************/
 #include "io.h"
-const char* color_strings[NUM_COLORS] = { "\x1b[31m", "\x1b[37m", "\x1b[0m" };
+
+const char *color_strings[NUM_COLORS] = {"\x1b[31m", "\x1b[37m", "\x1b[0m"};
+
 /**
  * function that does buffer clearing when there is a buffer overflow on
  * input - only call this function when there is an actual buffer overflow as
  * otherwise you will have additional problems
  **/
-void clear_buffer(void)
-{
-        int ch;
-        /* while there are still chars to read and we have not reached
-         * the newline char, keep reading */
-        while (ch = getc(stdin), ch != EOF && ch != '\n')
-                ;
-        /* reset the error status on the stdin file pointer */
-        clearerr(stdin);
+void clear_buffer(void) {
+	int ch;
+	/* while there are still chars to read and we have not reached
+	 * the newline char, keep reading */
+	while (ch = getc(stdin), ch != EOF && ch != '\n');
+	/* reset the error status on the stdin file pointer */
+	clearerr(stdin);
 }
 
 /**
@@ -29,14 +29,13 @@ void clear_buffer(void)
  * wish to change our output target (such as using a gui) we could change our
  * implementation here and it would not affect any other code in our project.
  **/
-int normal_print(const char format[], ...)
-{
-        int char_count;
-        va_list vlist;
-        va_start(vlist, format);
-        char_count = vprintf(format, vlist);
-        va_end(vlist);
-        return char_count;
+int normal_print(const char format[], ...) {
+	int char_count;
+	va_list vlist;
+	va_start(vlist, format);
+	char_count = vprintf(format, vlist);
+	va_end(vlist);
+	return char_count;
 }
 
 /**
@@ -44,21 +43,19 @@ int normal_print(const char format[], ...)
  * standard practice to output all error messages to stderr rather than
  * stdout (where printf sends messages).
  **/
-int error_print(const char format[], ...)
-{
-        int char_count = 0;
-        va_list vlist;
-        char_count += fprintf(stderr, "Error: ");
-        va_start(vlist, format);
-        char_count += vfprintf(stderr, format, vlist);
+int error_print(const char format[], ...) {
+	int char_count = 0;
+	va_list vlist;
+	char_count += fprintf(stderr, "Error: ");
+	va_start(vlist, format);
+	char_count += vfprintf(stderr, format, vlist);
 
-        return char_count;
+	return char_count;
 }
 
 /**
  * defines how to print the board given the orientation passed in. Pleas
  * see the asssignment specifications for further details.
  **/
-void board_print(board theboard, enum orientation orient)
-{
+void board_print(board theboard, enum orientation orient) {
 }
