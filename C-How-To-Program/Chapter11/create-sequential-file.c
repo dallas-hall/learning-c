0cf6e6bb01dd459725924a72fd234c2afbc69b17
Dallas Hall <dallas.hall@example.com>
2019-06-28 07:53:52 +1000

    Basic example on creating a sequential file in C.

---
+++ C-How-To-Program/Chapter11/create-sequential-file.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(void)
+{
+	char input;
+	int account;
+	char name[32];
+	double balance;
+
+	/*
+	 * FILE a pointer to a file.
+	 */ 
+	FILE *filePtr;
+
+	/*
+	 * w = write mode, will create a file if it doesn't exist and clobber a file if it does exist.
+	 */ 
+	if(NULL == (filePtr = fopen("clients.dat", "w"))) {
+		fprintf(stderr, "[ERROR] File could not be opened.\n");
+	}
+	else {
+		/*
+		 * Write the header to the file.
+		 */ 
+		fprintf(filePtr, "ACCOUNT_NUM;NAME;BALANCE\n");
+
+		while(1) {
+			printf("Do you want to enter an account? (y/n): ");
+			/*
+			 * Space to consume trailing newline from previous loop.
+			 */ 
+			scanf(" %c", &input);
+			if(tolower(input) == 'n') {
+				break;
+			}
+
+			printf("Enter account number: ");
+			scanf("%d", &account);
+			
+
+			printf("Enter name: ");
+			/*
+		 	* Space to consume the trailing newlinle
+		 	*/ 
+			scanf(" %s", name);
+
+			printf("Enter account balance: ");
+			scanf(" %lf", &balance);
+			fprintf(filePtr, "%d;%s;%.2f\n", account, name, balance);
+
+		}
+	}	
+
+	printf("Program done.\n");
+	return EXIT_SUCCESS;
+}
