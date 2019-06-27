a9ffc0b3a2e0f3079f22d31d50b7b159c49aa71d
Dallas Hall <mr.dallas.hall@gmail.com>
2019-06-27 17:03:15 +1000

    Field width format specifier examples.

--- C-How-To-Program/Chapter09/field-width-format-specifier.c
+++ C-How-To-Program/Chapter09/field-width-format-specifier.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-	puts("# Precision Width Format Specifiers");
+	puts("# Field Width Format Specifiers");
 	printf("%%4d 1 after \\t is\t%4d\n", 1);
 	printf("%%4d 12 after \\t is\t%4d\n", 12);
 	printf("%%4d 123 after \\t is\t%4d\n", 123);
