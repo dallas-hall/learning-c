be18d7641a3b5cc22a30662a43bb81502daf62f1
Dallas Hall <mr.dallas.hall@gmail.com>
2019-07-10 19:57:11 +1000

    Binary search tree example with extensive commenting.

--- C-How-To-Program/Chapter12/binary-search-tree-example.c
+++ C-How-To-Program/Chapter12/binary-search-tree-example.c
@@ -15,7 +15,7 @@ struct treeNode {
  * Create some synonyms.
  * 1) For the struct treeNode
  * 2) For the pointer to the struct treeNode
- */ 
+ */
 typedef struct treeNode TreeNode;
 typedef TreeNode *TreeNodePtr;
 
@@ -24,7 +24,7 @@ void inOrder(TreeNodePtr treePtr);
 void preOrder(TreeNodePtr treePtr);
 void postOrder(TreeNodePtr treePtr);
 void printMenu(void);
-void populateTree(TreeNodePtr treePtr);
+void populateTree(TreeNodePtr *treePtr);
 
 int main(void)
 {
@@ -33,14 +33,15 @@ int main(void)
 	/*
 	 * The binary search tree will start out empty.
 	 * NULL is the end of the nodes.
-	 */ 
+	 */
 	TreeNodePtr rootPtr = NULL;
 	puts("# Binary Search Tree Example");
 
 	/*
 	 * Put some capital letters into the tree
+	 * Pass the address of the pointer so we can directly manipulate it.
 	 */
-	populateTree(rootPtr);
+	populateTree(&rootPtr);
 
 	do {
 		printMenu();
@@ -65,7 +66,7 @@ int main(void)
 		}
 	}
 	while (!done);
-	
+
 	puts("Program exiting.");
 	return EXIT_SUCCESS;
 }
@@ -75,12 +76,12 @@ void insertTreeNode(TreeNodePtr *treePtr, char c)
 	if(*treePtr == NULL) {
 		/*
 		 * mallac tries to allocate memory with the specified bytes.
-	 	* sizeof will return the size of the data structure, platform dependent.
-	 	* If successful, malloc returns a void * to the allocated memory, otherwise returns NULL
-	 	*/
-		treePtr = malloc(sizeof(TreeNode));
-		
-		if(treePtr != NULL) {
+	 	 * sizeof will return the size of the data structure, platform dependent.
+	 	 * If successful, malloc returns a void * to the allocated memory, otherwise returns NULL
+	 	 */
+		*treePtr = malloc(sizeof(TreeNode));
+
+		if(*treePtr != NULL) {
 			/*
 			 * The root (first) node of the tree becomes the middle value.
 		 	 */
@@ -97,13 +98,20 @@ void insertTreeNode(TreeNodePtr *treePtr, char c)
 		 * Values smaller than the root node are added to the left hand side of the tree
 		 */
 		if(c < (*treePtr)-> data) {
-			insertTreeNode( &((*treePtr)->leftTreeNodePtr), c);
+			/*
+			 * Recursively call insertTreeNode with the address of leftTreeNode pointer, so we can insert it there.
+			 */ 
+			insertTreeNode(&((*treePtr)->leftTreeNodePtr), c);
 		}
 		/*
 		 * Values larger than the root node are added to the right hand side of the tree
-		 */ 
+		 */
 		else if(c > (*treePtr)-> data) {
-			insertTreeNode( &((*treePtr)->rightTreeNodePtr), c);
+			/*
+			 * Recursively call insertTreeNode with the address of rightTreeNode pointer, so we can insert it there.
+			 */ 
+	
+			insertTreeNode(&((*treePtr)->rightTreeNodePtr), c);
 		}
 		else {
 			puts("Duplicate values aren't inserted into the tree.");
@@ -111,6 +119,12 @@ void insertTreeNode(TreeNodePtr *treePtr, char c)
 	}
 }
 
+/*
+ * Print in ascending order.
+ * 1) Left nodes and children first (lowest)
+ * 2) Middle (root) node
+ * 3) Right nodes and children last (highest)
+ */ 
 void inOrder(TreeNodePtr treePtr)
 {
 	if(treePtr != NULL) {
@@ -119,11 +133,36 @@ void inOrder(TreeNodePtr treePtr)
 		inOrder(treePtr->rightTreeNodePtr);
 	}
 }
+
+/*
+ * Print the value of each node as its visited.
+ * 1) Root
+ * 2) left child
+ * 3) Right child
+ * 4) Repeat 1 to 3 for all nodes.
+ */ 
 void preOrder(TreeNodePtr treePtr)
 {
+	if(treePtr != NULL) {
+		printf("%c ", treePtr->data);
+		preOrder(treePtr->leftTreeNodePtr);
+		preOrder(treePtr->rightTreeNodePtr);
+	}
 }
+
+/*
+ * Print children node values first.
+ * 1) All left node children then left nodes
+ * 2) All right node children then right nodes
+ * 3) Middle (root) value
+ */ 
 void postOrder(TreeNodePtr treePtr)
 {
+	if(treePtr != NULL) {
+		preOrder(treePtr->leftTreeNodePtr);
+		preOrder(treePtr->rightTreeNodePtr);
+		printf("%c ", treePtr->data);
+	}
 }
 
 void printMenu(void)
@@ -137,14 +176,16 @@ void printMenu(void)
 	printf("Enter the number for yor choice: ");
 }
 
-void populateTree(TreeNodePtr treePtr)
+void populateTree(TreeNodePtr *treePtr)
 {
 	int i;
 	char c;
 
+	puts("## Populating Binary Search Tree");
+
 	for(i = 0; i < 10; i++) {
 		c = 65 + rand() % 25;
-		printf("%c\t", c);
-		/*insertTreeNode(treePtr, c);*/
+		printf("%c\n", c);
+		insertTreeNode(treePtr, c);
 	}
 }
