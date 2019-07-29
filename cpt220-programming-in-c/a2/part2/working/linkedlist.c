277b817d052b820fd1250846a53edcecee36fa47
Dallas Hall <mr.dallas.hall@gmail.com>
2019-07-30 05:18:32 +1000

    Basic linked list implementation added.

--- cpt220-programming-in-c/a2/part2/working/linkedlist.c
+++ cpt220-programming-in-c/a2/part2/working/linkedlist.c
@@ -1,4 +1,3 @@
-#include "linkedlist.h"
 /******************************************************************************
  * Student Name    :  Dallas Hall
  * RMIT Student ID :  s3461243
@@ -8,6 +7,63 @@
  * study period 2, 2019.
  *****************************************************************************/
 
+#include "linkedlist.h"
+
 /**
  * implement the functions for managing the linked list here
  **/
+/*
+ * My linked list code comes from week 8 and 9 tutorial code as well
+ * as C How To Program 6e chapter 12.
+ */
+struct linkedlist* createLinkedList(struct linkedlist* theLinkedListPtr)
+{
+	/*
+	 * mallac tries to allocate memory with the specified bytes.
+	 * sizeof will return the size of the data structure, platform dependent.
+	 * If successful, malloc returns a void * to the allocated memory, otherwise
+	 * returns NULL
+	 *
+	 * We are using struct linkedlist because that is the object that will be
+	 * stored in the pointer.
+	 */
+	theLinkedListPtr = malloc(sizeof(struct linkedlist));
+
+	/*
+	 * Same as theLinkedListPtr == NULL
+	 */
+	if(!theLinkedListPtr) {
+		return NULL;
+	}
+	else {
+		theLinkedListPtr->head = NULL;
+		theLinkedListPtr->size = 0;
+	}
+
+	return theLinkedListPtr;
+}
+
+BOOLEAN insertNode(struct linkedlist* theLinkedListPtr, struct node* theNodePtr)
+{
+	return FALSE;
+}
+
+BOOLEAN deleteNode(struct linkedlist* theLinkedListPtr, struct node* theNodePtr)
+{
+	return FALSE;
+}
+
+void printLinkedList(struct linkedlist* theLinkedListPtr)
+{
+
+}
+
+BOOLEAN saveLinkedList(struct linkedlist*  theLinkedList, FILE* outputFilePtr)
+{
+	return FALSE;
+}
+
+BOOLEAN deleteLinkedList(struct linkedlist* theLinkedListPtr)
+{
+	return FALSE;
+}
