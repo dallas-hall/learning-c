6085705541e195ec33fe4a82699f7325456b2139
Dallas Hall <dallas.hall@example.com>
2019-08-03 15:14:06 +1000

    Updating the scoreboard after a game has finished with a winner. Needs testing still.

--- cpt220-programming-in-c/a2/part2/working/main.h
+++ cpt220-programming-in-c/a2/part2/working/main.h
@@ -108,7 +108,17 @@ void quit_program(struct game_system*);
 
 void abort_program(struct game_system*);
 
-struct game_system* createGameSystemPtr(void);
+struct game_system* createGameSystem(void);
+
+struct linkedlist* getLinkedList(void);
+
+struct game_system* getGameSystem(void);
+
+/*
+ * Used to print the game system state for debugging.
+ */
+void printDebugGameSystem(struct game_system* theGameSystemPtr);
+
 /**
  * end of assignment 2 data structures
  **/
@@ -117,8 +127,3 @@ struct game_system* createGameSystemPtr(void);
  * You may add your own datastructures or function declarations here if you
  * need to.
  **/
-
-/*
- * Used to print the game system state for debugging.
- */
-void printDebugGameSystem(struct game_system* theGameSystemPtr);
