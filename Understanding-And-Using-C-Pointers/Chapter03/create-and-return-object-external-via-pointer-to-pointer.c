e0f5c13c324a78d1003c6891cac4c1ccdbc71695
Dallas Hall <dallas.hall@example.com>
2019-08-14 19:19:20 +1000

    Creating objects outside a function, passing it via a pointer to a pointer to be updated, and returning it via a pointer.

---
+++ Understanding-And-Using-C-Pointers/Chapter03/create-and-return-object-external-via-pointer-to-pointer.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ARRAY_SIZE 8
+
+int* allocateArray(int**, int);
+
+int main(void)
+{
+	int* array;
+
+	puts("# Creating & Returning Objects - External Object Creation & Pointer To A Pointer");
+	puts("When creating an object externally outsidea function with malloc, the caller is still responsible for using free on the object.");
+	puts("When passing a pointer to a pointer to aType, we need to pass in the address of the pointer to aType, and dereference the pointer to a pointer to aType inside the function to manipulate the pointer to aType");
+
+	array = (int*) malloc(sizeof(int) * (unsigned long) ARRAY_SIZE);
+
+	if(!array) {
+		return EXIT_FAILURE;
+	}
+	
+	/*
+	 * We must use the address of the pointer to a pointer to aType.
+	 * This is so it matches the aType** parameter type.
+	 */ 
+	allocateArray(&array, ARRAY_SIZE);
+
+	for(int i = 0; i < ARRAY_SIZE; i++) {
+		printf("i is %d and has the value %d\n", i, array[i]);
+	}
+
+	free(array);
+
+	return EXIT_SUCCESS;
+}
+
+int* allocateArray(int** array, int size)
+{
+	for(int i = 0; i < size; i++) {
+		/*
+		 * When updating a pointer to a pointer to aType, we must dereference it.
+		 * This is so we are updating the pointer to Atype.
+		 *
+		 * Since we are working with a pointer to a pointer to int.
+		 * To get the address of the pointer to an int, we must deference.
+		 *
+		 * (*array)
+		 *
+		 * To get the value from that pointer to an int, we must dereference again.
+		 * *(*array)
+		 *
+		 * To iterate through all of the address inside array, we add i each time we go through the loop.
+		 */ 
+		*(*array + i) = i * i;
+	}
+
+	/*
+	 * We must dereference again to return a pointer to Atype
+	 */ 
+	return *array;
+}
