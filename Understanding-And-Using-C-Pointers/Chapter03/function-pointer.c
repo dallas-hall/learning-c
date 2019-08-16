d3c672bdaae196e0e317f3e87ea27adc197d7717
Dallas Hall <dallas.hall@example.com>
2019-08-16 11:41:32 +1000

    A basic function pointer example.

---
+++ Understanding-And-Using-C-Pointers/Chapter03/function-pointer.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int squareInt(int n);
+double squareDouble(double n);
+
+int main(void)
+{
+	/*
+	 * Declare the function pointers
+	 * fptr is a function pointer that takes an int and returns an int.
+	 * We must provide a function's name that matches this signature. The functions name is a pointer itself.
+	 */
+	int (*fptr1)(int);
+	double (*fptr2)(double);
+
+	int i = 2;
+	double d = 3.14;
+
+	puts("# Functoin Pointers");
+
+	printf("The value is i is %d\n", i);
+	/*
+	 * Assign the address of the function to the function pointer
+	 */ 
+	fptr1 = squareInt;
+	/*
+	 * Call the function via the function pointer.
+	 */ 
+	i = fptr1(i);
+	printf("After squareInt, the value is i is %d\n", i);
+	
+	printf("The value is d is %f\n", d);
+	fptr2 = squareDouble;
+	d = fptr2(d);
+	printf("After squareInt, the value is i is %f\n", d);
+
+	return EXIT_SUCCESS;
+}
+
+int squareInt(int n)
+{
+	return n * n;
+}
+
+double squareDouble(double n)
+{
+	return n * n;
+}
