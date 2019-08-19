078b56883aa4af0f74cc8988ab7fbce240fcec73
Dallas Hall <dallas.hall@example.com>
2019-08-19 19:28:09 +1000

    Fixed ^D program exit during playing a game.

--- cpt220-programming-in-c/a2/part3/working/game.c
+++ cpt220-programming-in-c/a2/part3/working/game.c
@@ -111,6 +111,10 @@ void play_game(struct game_system* gameSystemPtr)
 			else {
 				winner = loser = NULL;
 				normal_print("\nThe game was a draw!\n");
+				/*
+				 * Need clear_buffer here so the program doesn't exit.
+				 */
+				clear_buffer();
 				return;
 			}
 			normal_print(
@@ -127,6 +131,10 @@ void play_game(struct game_system* gameSystemPtr)
 			gameSystemPtr->theresult = *gameResultPtr;
 			free(gameResultPtr);
 
+			/*
+			 * Need clear_buffer here so the program doesn't exit.
+			 */
+			clear_buffer();
 			return;
 		}
 		/* otherwise, check if the game has actually been won and if so
