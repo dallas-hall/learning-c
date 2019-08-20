338b74c56ce12b353726776fa30893616e2c3335
Dallas Hall <dallas.hall@example.com>
2019-08-20 13:18:29 +1000

    Fixed bugs.

--- C-How-To-Program/Chapter08/strcmp-and-strncmp.c
+++ C-How-To-Program/Chapter08/strcmp-and-strncmp.c
@@ -9,23 +9,23 @@ int main(void)
 	char s2[] = "hello world!";
 	
 	puts("# String Comparison Functions");
-	puts("## strcmp(s2, s1) - lexicographically compare s1 to s2");
+	puts("## strcmp(s1, s2) - lexicographically compare s1 to s2");
 	printf("s1 is \"%s\"\ns2 is \"%s\"\nLexicographically comparison result is %d\n", s1, s2, strcmp(s1, s2));
 	
 	puts("\n## strcmp(s2, s1) - lexicographically compare s2 to s1");
-	printf("s1 is \"%s\"\ns2 is \"%s\"\nLexicographically comparison result is %d\n", s1, s2, strcmp(s2, s1));
+	printf("s1 is \"%s\"\ns2 is \"%s\"\nLexicographical comparison result is %d\n", s1, s2, strcmp(s2, s1));
 	
 	puts("\n## strcmp(s1, s1) - lexicographically compare s1 to s1");
-	printf("s1 is \"%s\"\nLexicographically comparison result is %d\n", s1, strcmp(s1, s1));
+	printf("s1 is \"%s\"\nLexicographical comparison result is %d\n", s1, strcmp(s1, s1));
 	
-	puts("\n## strncmp(s2, s1) - lexicographically compare s1 to s2 with first 5 chars");
-	printf("s1 is \"%s\"\ns2 is \"%s\"\nLexicographically comparison result is %d\n", s1, s2, strncmp(s1, s2, 5));
+	puts("\n## strncmp(s1, s2, 5) - lexicographically compare s1 to s2 with first 5 chars");
+	printf("s1 is \"%s\"\ns2 is \"%s\"\nLexicographical comparison result is %d\n", s1, s2, strncmp(s1, s2, 5));
 	
-	puts("\n## strncmp(s2, s1) - lexicographically compare s2 to s1 with first 5 chars");
-	printf("s1 is \"%s\"\ns2 is \"%s\"\nLexicographically comparison result is %d\n", s1, s2, strncmp(s2, s1, 5));
+	puts("\n## strncmp(s2, s1, 5) - lexicographically compare s2 to s1 with first 5 chars");
+	printf("s1 is \"%s\"\ns2 is \"%s\"\nLexicographical comparison result is %d\n", s1, s2, strncmp(s2, s1, 5));
 	
-	puts("\n## strncmp(s1, s1) - lexicographically compare s1 to s1 with first 5 chars");
-	printf("s1 is \"%s\"\nLexicographically comparison result is %d\n", s1, strncmp(s1, s1, 5));
+	puts("\n## strncmp(s1, s1, 5) - lexicographically compare s1 to s1 with first 5 chars");
+	printf("s1 is \"%s\"\nLexicographical comparison result is %d\n", s1, strncmp(s1, s1, 5));
 
 
 
