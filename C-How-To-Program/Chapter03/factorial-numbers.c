07fbb0676db135242cba065d4b904469a837d35b
Dallas Hall <mr.dallas.hall@gmail.com>
2019-09-08 12:09:11 +1000

    Iterative factorial numbers.

---
+++ C-How-To-Program/Chapter03/factorial-numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	unsigned long int total;
+	int input, number;
+
+	//https://www.mathsisfun.com/numbers/factorial.html
+	puts("# Factorial Numbers");
+	do {
+		printf("Enter an integer greater than 0: ");
+		scanf("%d", &input);
+	}
+	while (input <= 0);
+
+	total = 1;
+	if (input != 1) {
+		number = 1;
+		while (number <= input) {
+			total *= (unsigned long int) number;
+			++number;
+		}
+	}
+	printf("%d! = %lu\n", input, total);
+
+	return EXIT_SUCCESS;
+}
