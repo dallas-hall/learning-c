78b8b3c29d6755076289410903bb867bcb649094
Dallas Hall <mr.dallas.hall@gmail.com>
2019-09-13 07:55:00 +1000

    Can't spell lolz.

--- C-How-To-Program/Chapter05/fibonacci-recursion.c
+++ C-How-To-Program/Chapter05/fibonacci-recursion.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	unsigned int printCounter = 0;
 
-	puts("# Fibonnaci Series - Recursion");
+	puts("# Fibonacci Series - Recursion");
 
-	printf("The fibonnaci series up to %d is ", LIMIT);
+	printf("The fibonacci series up to %d is ", LIMIT);
 	for(int i = 0; i <= LIMIT; i++) {
 		printf("%llu ", fibonnaci(i));
 		++printCounter;
@@ -26,11 +26,11 @@ int main(void)
 }
 
 /*
- * Fibonnaci begins with 0 and 1. 0 + 1 = 1.
+ * Fibonacci begins with 0 and 1. 0 + 1 = 1.
  * The next number is taken add added to the result, so 1 + 1 = 2. And so on.
  * The recursive formula is fib(n - 1) + fib (n - 2);
  */ 
-unsigned long long fibonnaci(int n)
+unsigned long long fibonacci(int n)
 {
 	unsigned long long recursionResult1, recursionResult2, finalResult;
 	
