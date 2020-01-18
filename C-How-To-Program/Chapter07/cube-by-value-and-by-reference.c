4f62e789b6a1fbfd7c89309de28a2f7e50d22f3b
Dallas Hall <dallas.hall@example.com>
2020-01-18 21:55:15 +1100

    Added some clarifying comments.

--- C-How-To-Program/Chapter07/cube-by-value-and-by-reference.c
+++ C-How-To-Program/Chapter07/cube-by-value-and-by-reference.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int cubeByValue(int n);
-void cubeByReference(int *nPointer);
+void cubeByReference(int* nPointer);
 
 int main(void)
 {
@@ -10,28 +10,27 @@ int main(void)
 
 	printf("# Cube Number - Pass By Value\nEnter an integer: ");
 	scanf("%d", &input);
-	printf("The original number (input) is %d\n", input);
+	printf("The original number (input) before cubeByValue is %d\n", input);
 	result = cubeByValue(input);
 	printf("The original number (input) after cubeByValue is %d\n", input);
 	printf("The new number (result) after cubeByValue is %d\n", result);
 
 	printf("\n# Cube Number - Simulate Pass By Reference\nEnter an integer: ");
 	scanf("%d", &input);
-	printf("The original number (input) is %d\n", input);
+	printf("The original number (input) before cubeByReference is %d\n", input);
 	/*
 	 * The & is needed here to pass the memory address of the variable.
 	 * This simulates call by reference.
-	 */ 
+	 */
 	cubeByReference(&input);
 	printf("The original number (input) after cubeByReference is %d\n", input);
-
 
 	return EXIT_SUCCESS;
 }
 
 /*
  * We need a return because we are manipulating the value.
- */ 
+ */
 int cubeByValue(int n)
 {
 	return n * n * n;
@@ -39,8 +38,12 @@ int cubeByValue(int n)
 
 /*
  * We don't need a return because we are directly manipulating the value.
- */ 
-void cubeByReference(int *nPointer)
+ * The argument type is a pointer so we can simulate pass by reference and directly manipulate the value the pointer is pointing to.
+ */
+void cubeByReference(int* nPointer)
 {
+	/*
+	 * dereference = dereference multiplied by dereference multiplied by dereference
+	 */
 	*nPointer = *nPointer * *nPointer * *nPointer;
 }
